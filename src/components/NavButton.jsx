b2e02c50242dccbdb72f0825fb8e640df0021547
Leonardo <leo43161301@gmail.com>
2021-12-22 10:16:07 -0300

    Se empezo a darle un estilo al header, a los botones del navbar y el nombre

--- src/components/NavButton.jsx
+++ src/components/NavButton.jsx
@@ -3,10 +3,10 @@ import { Link } from 'react-scroll'
 
 export default function NavButton({ to, icon, text }) {
     return (
-        <Link activeClass="active" to={to} spy={true} duration={500} >
-            <div className="d-flex justify-content-between btn btn-danger">
+        <Link activeClass="active" to={to} spy={true} duration={500} className="nav-button">
+            <div className="d-flex justify-content-between">
                 <div>{icon}</div>
-                <div>{text}</div>
+                <span className="nav-text">{text}</span>
             </div>
         </Link>
     )
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import NavButton from './NavButton'
+import Navbar from 'react-bootstrap/Navbar'
 
 export default function Navigation() {
     return (
-        <nav className="d-flex sticky-top">
+        <Navbar expand="lg" sticky="top" className="flex-column text-white">
+            <div className="d-flex px-2 align-items-end w-100 my-4 px-4 flex-column">
+                <h5 className="mb-1">Palavecino Leonardo</h5>
+                <p className="m-0">Programador Web</p>
+            </div>
             <div className="d-flex flex-column w-100">
                 <NavButton to="intro" icon="1" text="Intro"></NavButton>
-                <NavButton to="portfolio" icon="1" text="Intro"></NavButton>
+                <NavButton to="portfolio" icon="1" text="Portfolio"></NavButton>
                 <NavButton to="sobreMi" icon="1" text="Intro"></NavButton>
                 <NavButton to="contacto" icon="1" text="Intro"></NavButton>
             </div>
-        </nav>
+        </Navbar>
     )
 }
