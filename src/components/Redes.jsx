e2b32266219202d86aa17a705796c7ecffe1918b
Leonardo <leo43161301@gmail.com>
2021-12-27 15:25:57 -0300

    Se mejoro el responsive de las redes sociales y se agrego efectos dichos botones

--- src/components/Redes.jsx
+++ src/components/Redes.jsx
@@ -4,10 +4,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 export default function Redes({ expand }) {
     return (
         <div className={`${expand ? "d-flex d-md-none" : "d-md-flex d-none"} justify-content-evenly py-3`}>
-            <div><FontAwesomeIcon icon={['fab', 'twitter']}></FontAwesomeIcon></div>
-            <div><FontAwesomeIcon icon={['fab', 'facebook-f']}></FontAwesomeIcon></div>
-            <div><FontAwesomeIcon icon={['fab', 'github']}></FontAwesomeIcon></div>
-            <div><FontAwesomeIcon icon={"envelope"}></FontAwesomeIcon></div>
+            <div className="redes-button"><FontAwesomeIcon icon={['fab', 'twitter']}></FontAwesomeIcon></div>
+            <div className="redes-button"><FontAwesomeIcon icon={['fab', 'facebook-f']}></FontAwesomeIcon></div>
+            <div className="redes-button"><FontAwesomeIcon icon={['fab', 'github']}></FontAwesomeIcon></div>
+            <div className="redes-button"><FontAwesomeIcon icon={"envelope"}></FontAwesomeIcon></div>
         </div>
     )
 }
--- src/pages/Navigation.jsx
+++ src/pages/Navigation.jsx
@@ -8,24 +8,28 @@ export default function Navigation() {
     const [expanded, setExpanded] = useState(false);
     return (
         <Navbar expand="md" expanded={expanded} sticky="top" className="text-white py-0">
-            <Container className="p-0 flex-md-column">
-                <div className="d-flex justify-content-between justify-content-md-end w-100 px-lg-3 my-lg-4 px-3 py-3">
-                    <Navbar.Toggle onClick={() => setExpanded(!expanded)} className="h-100 my-auto" aria-controls="basic-navbar-nav" />
-                    <div className="d-flex px-2 align-items-end flex-column text-end">
-                        <h5 className="mb-1">Palavecino Leonardo</h5>
-                        <p className="m-0">Programador Web</p>
+            <Container className="p-0 flex-md-column justify-content-between h-100">
+                <div className="p-0 flex-md-column w-100">
+                    <div className="d-flex justify-content-between justify-content-md-end w-100 px-lg-3 my-lg-4 px-3 py-3">
+                        <Navbar.Toggle onClick={() => setExpanded(!expanded)} className="h-100 my-auto border-white" aria-controls="basic-navbar-nav" />
+                        <div className="d-flex px-2 align-items-end flex-column text-end">
+                            <h5 className="mb-1">Palavecino Leonardo</h5>
+                            <p className="m-0">Programador Web</p>
+                        </div>
                     </div>
+                    <Navbar.Collapse id="basic-navbar-nav" className="w-100">
+                        <div className="d-flex flex-column w-100">
+                            <NavButton setExpanded={setExpanded} to="intro" icon="home" text="Intro"></NavButton>
+                            <NavButton setExpanded={setExpanded} to="portfolio" icon="th" text="Portfolio"></NavButton>
+                            <NavButton setExpanded={setExpanded} to="sobreMi" icon="user" text="Sobre Mi"></NavButton>
+                            <NavButton setExpanded={setExpanded} to="contacto" icon="envelope" text="Contacto"></NavButton>
+                            <Redes expand={true}></Redes>
+                        </div>
+                    </Navbar.Collapse>
                 </div>
-                <Navbar.Collapse id="basic-navbar-nav" className="w-100">
-                    <div className="d-flex flex-column w-100">
-                        <NavButton setExpanded={setExpanded} to="intro" icon="home" text="Intro"></NavButton>
-                        <NavButton setExpanded={setExpanded} to="portfolio" icon="th" text="Portfolio"></NavButton>
-                        <NavButton setExpanded={setExpanded} to="sobreMi" icon="user" text="Sobre Mi"></NavButton>
-                        <NavButton setExpanded={setExpanded} to="contacto" icon="envelope" text="Contacto"></NavButton>
-                        <Redes expand={true}></Redes>
-                        <Redes></Redes>
-                    </div>
-                </Navbar.Collapse>
+                <div className="w-100 border-top border-secondary">
+                    <Redes></Redes>
+                </div>
             </Container>
         </Navbar>
     );
