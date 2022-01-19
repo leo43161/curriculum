3e109d8dce2742103fa0307f1c3498e2f1fbe40a
Leonardo <leo43161301@gmail.com>
2022-01-19 10:54:33 -0300

    Se mejoro el scroll automatico para la version movil

--- src/components/NavButton.jsx
+++ src/components/NavButton.jsx
@@ -3,8 +3,10 @@ import { Link } from 'react-scroll';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function NavButton({ to, icon, text, setExpanded }) {
+    const width = window.screen.width;
+    const offserConfig = width < 430 ? -350 : 0;
     return (
-        <Link activeClass="active" to={to} spy={true} smooth={true} duration={500} offset={-20} className="nav-button">
+        <Link activeClass="active" to={to} spy={true} smooth={true} duration={500} offset={offserConfig} className="nav-button">
             <div className="d-flex justify-content-between flex-row-reverse flex-md-row" onClick={() => setExpanded(false)}>
                 <div><FontAwesomeIcon icon={icon}></FontAwesomeIcon></div>
                 <span className="nav-text">{text}</span>
