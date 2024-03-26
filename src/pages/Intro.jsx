2174f641538dbad5f679999410548f9e77603964
Leonardo Palavecino <64588981+leo43161@users.noreply.github.com>
2024-03-26 15:13:28 -0300

    se verifico el build y se agrego un color

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -9,7 +9,7 @@ export default function Intro() {
         <div className="d-flex justify-content-center align-items-center intro text-dark">
             <div className="col-lg-10 col-md-10 col-11 text-center text-white text-intro">
                 <h1 className='text-start text-title-1'>Hola! soy Leonardo Palavecino</h1>
-                <h1 className='text-start text-title-2'>Programador FullStack</h1>
+                <h1 className='text-color-secondary text-start text-title-2'>Programador FullStack</h1>
                 <h2 className="mb-4 text-start text-title-3">Este es mi portfolio online</h2>
                 {/* <Link
                     to="portfolio"
